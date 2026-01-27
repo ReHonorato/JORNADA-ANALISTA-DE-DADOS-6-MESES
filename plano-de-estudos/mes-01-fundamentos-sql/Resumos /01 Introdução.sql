816c39e8d2cbe40fac161139c41687e59b5017d8
Renata Honorato <re_naathy@hotmail.com>
2026-01-27 20:03:16 -0300

    Refine SQL introduction and data handling explanations

--- plano-de-estudos/mes-01-fundamentos-sql/Resumos /01 Introdução.sql
+++ plano-de-estudos/mes-01-fundamentos-sql/Resumos /01 Introdução.sql
@@ -8,17 +8,23 @@ A analise de dados consiste em coletar, organizar, tratar e interpretar dados br
 -- O QUE SÃO DADOS BRUTOS?
 -- ==================================================================================
 
-São registros soltos de algo. De forma bruta eles não significam nada.
+São registros soltos sobre algo que de forma bruta não significam nada.
 
-Um exemplo simples; se encontrassemos varias peças de um quebra-cabeça, seria impossivel de adivinhar qual a imagem so de olhar pra eles. Os dados brutos são dessa forma, a amenos que eles sejam organizados não podemos fazer uma leitura de informação. Os dados so passam a ter significado quando estão organizados.
+Um exemplo simples; se encontrassemos varias peças de um quebra-cabeça, seria impossivel 
+  de adivinhar qual seria a a imagem so de olhar pra eles. Os dados brutos são dessa forma,
+  a amenos que eles sejam organizados não podemos fazer uma leitura de informação. 
+  Os dados so passam a ter significado quando estão organizados.
 
 -- ==================================================================================
 -- ETAPAS E FERRRAMENTAS
 -- ==================================================================================
 
-COLETA = os dados estão armazenados de diversas formas, por exemplo: eles podem estar em planilhas do excel, podem estar em banco de dados de sistemas ou em arquivos CSV. Para coletar vamos usar o Excel, consultas com SQL ou Python.
+COLETA = os dados estão armazenados de diversas formas, por exemplo: eles podem estar 
+em planilhas do excel, podem estar em banco de dados de sistemas ou em arquivos CSV. 
+Para coletar vamos usar o Excel, consultas com SQL ou Python.
 
-LIMPEZA = _ Os dados sempre vem com erros, campos vazios e duplicidades. Para resolver isso podemos usar as seguintes ferramentas:
+LIMPEZA = _ Os dados sempre vem com erros, campos vazios e duplicidades. Para resolver isso 
+podemos usar as seguintes ferramentas:
 
 Excell -> remover duplicatas, ajustar a formatação, corrigir valores incorrtos etc.
 
@@ -28,9 +34,9 @@ Pandas -> corrigir valores nulo, corrigir tipo de dados,filtrar erros em grande 
 
 ORGANIZAR = _criar estrutura
 
-Sql -> juntar tabema e criar visões organizadas. 
-Excel ->separação pcriar novas colunas, agrupar dadosor categoria 
-Python -> criar no bovas tabelas, agrupar dados reorganizar tabelas
+Sql -> juntar tabelas e criar visões organizadas de dados 
+Excel ->separação por categoria  e tabelas estruturadas. 
+Python -> criar novas colunas, agrupar dados e reorganizar tabelas
 
 ANALISE = responder perguntas de negócio usando os dados. O QUE? POR QUE?
   
@@ -38,7 +44,7 @@ Sql -> Calculo de metricas (SUM, AVG, COUNT) e agrupamento de categorias.
 Excel -> Tabelas dinamicas e formulas analiticas
 Python -> Analise exploratória (EDA), estatística e Identificação de padrões
 
-INSIGHTS = é aqui demosntramos o que vai acontecer e quais as melhores decisões a tomar
+INSIGHTS = é nessa etapa que demosntramos o que vai acontecer e quais as melhores decisões a tomar
 
 Power BI -> Dashboards interativos, Indicadores (KPIs) e visualização profissional de dados
 Excel -> Relatórios visuais e paineis de acompanhamento (graficos e dashbords)
