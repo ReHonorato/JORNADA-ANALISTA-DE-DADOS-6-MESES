5b853550947dd5f6a154ea82f4b9b5fb8310e756
Renata Honorato <re_naathy@hotmail.com>
2026-02-06 02:02:56 -0300

    Atualizar o Conceitos de Bancos relacionais.sql
    Modelagem

--- plano-de-estudos/mes-01-fundamentos-sql/Resumos /Conceitos de Bancos relacionais.sql
+++ plano-de-estudos/mes-01-fundamentos-sql/Resumos /Conceitos de Bancos relacionais.sql
@@ -1,8 +1,7 @@
 O QUE SÃO BANCO DE DADOS RELACIONAIS?
 
 Banco de dados são conjuntos de tabelas.
-Essas tabelas possuel linhas e colunas.
-Um bando de dados relacionais e um conjunto de tabelas que se conectam entre si atraves de conectores em comum.
+Um banco de dados relacional é um conjunto de tabelas que se conectam entre si atraves de conectores em comum.
 
 Em sua estrutura basica uma tabela possui linhas e colunas.
 
@@ -15,17 +14,17 @@ COLUNA ( field/Atribulto) - é  a categoria do dado.
 
 RELACIONAMENTOS
 
-Para que ocorra um relacionamento entre tabelas elas precisam ter um tipo de dado em comum, que causa uma conexao podendo cruzar dados entre uma e outra. Essas dados sao chamados de CHAVES.
+Para que ocorra um relacionamento entre tabelas elas precisam ter um tipo de dado em comum, que causa uma conexão podendo cruzar dados entre uma e outra. Essas dados são chamados de CHAVES.
 As chaves podem ser de dois tipos:
 
-PRIMARY KEY (Chave Primaria) - sao registros em uma coluna que nunca se repetem e nunca sao vazios, e como o RG da linha.
+PRIMARY KEY (Chave Primaria) - são registros em uma coluna que nunca se repetem e nunca são vazios, é como o RG da linha.
 
 FOREING KEY ( Chave estrangeira) - é quando a chave Primaria de uma tabela aparecem em outra para criar vinculo.
 
 
 TIPOS DE RELACIONAMENTOS
 
-Existem tres formas principais de as tabelas conversarem.
+Existem três formas principais de as tabelas conversarem.
 
 1:1 - E quando um registro de uma tabela esta ligado a apenas um registro em outra tabela. E uma forma rara de relacionamento visto que se ha apenas um registro ele poderia estar na mesma tabela, porem é utilizado para deixar a tabela mais leve ou ocultar dados sensíveis.
 
@@ -35,7 +34,7 @@ N:N - Este sem duvida e o mais complicado, pois ocorre quando varios registros a
 
 SGBD - Sistema Gerenciador de Banco de Dados.
 
-Vimos que utilizamos a Linguagem SQL para manipular os dados. Essas linguagem sera usada numa interface de um sigma que possibilita a manipulacao dos dados.
+Utilizamos a Linguagem SQL para manipular os dados. Essas linguagem será usada numa interface que possibilita a manipulação dos dados.
 
 Todo SGBD possui um servidor e uma interface.
 SGBDs mais comuns.
@@ -47,5 +46,16 @@ Oracle
 
 Podemos utilizar o SQl em todos eles.
 
+
+MODELAGEM DE DADOS
+
+O processo de modelagem tem três fases principais:
+
+CONCEITUAL: Aqui voce vai pensar no negócio. que tipos de dados quero guardar? Desenhe caixas com o nome das entidades.Pergunte-se; colo elas interagem? defina os relacionamentos entre as entidades.
+
+LOGICO: Nesta etapa definimos as Colunas e as chaves (PK, FK)
+
+FISICO: Transformar a logica para os códigos.  Aqui vamos usar um SGBD.
+Primeiro criamos a tabela utilizando o comando SQL CREATE TABLE e define a tipagem dos dados (INT, VARCHAR, DATE..)
 
 
